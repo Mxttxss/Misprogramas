91649aaf25b947cc89f321eeabaa55893c764d72
Mxttxss <matiasubl10@gmail.com>
2026-03-08 14:00:39 -0600

    Add files via upload

---
+++ Longitud.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main() {
+    
+    int n;
+    printf("Ingrese la cantidad de numeros: ");
+    scanf("%d", &n);
+
+    int array[n];
+    for (int i=0;i<n;i++) {
+        printf("Ingrese un numero: ");
+        scanf("%d", &array[i]);
+    }
+
+    int intervalo = 0;
+    int longitud = 0;
+
+    for (int i=0;i<n-1;i++) {
+        if (array[i] > array[i + 1]) {
+            longitud += array[i] - array[i + 1];
+        } else {
+            longitud = (longitud > intervalo) ? longitud : intervalo;
+            longitud = 0;
+        }
+    }
+
+    intervalo = (intervalo > longitud) ? intervalo : longitud;
+
+    printf("La longitud mas grande es: %d\n", longitud);
+
+    return 0;
+}
